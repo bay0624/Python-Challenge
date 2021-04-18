0e653f40b6e4982515cb469432d444cc83db4c5f
ABAYOMI OLUJOBI <abayomi@ABAYOMIs-iMac.local>
2021-04-18 13:21:24 -0400

    Add Python scripts

---
+++ PyPoll/main.py
@@ -0,0 +1,14 @@
+"""
+A script to run analysis for the PyPoll Data.
+
+A Python script that analyzes a set of polling data called election_data.csv.
+
+This script will analyze the votes and calculate the following:
+1. The total number of votes cast.
+2. A complete list of candidates who received votes.
+3. The percentage of votes each candidate won.
+4. The total number of votes each candidate won.
+5. The winner of the election based on popular vote.
+
+Author: Abayomi Olujobi
+"""
