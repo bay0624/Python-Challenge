af4a6fdac04e294f9b084033cfc87f9b4e31cb78
ABAYOMI OLUJOBI <abayomi@ABAYOMIs-iMac.local>
2021-04-18 21:58:18 -0400

    Added comments ot script

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,7 +8,7 @@ This script will analyze the records and calculate the following:
 2. The net total amount of "Profit/Losses" over the entire period.
 3. Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes.
 4. The greatest increase in profits (date and amount) over the entire period.
-5. The greatest decrease in losses (date and amount) over the entire period
+5. The greatest decrease in losses (date and amount) over the entire period.
 
 Author: Abayomi Olujobi
 """
