ca4e2e8a0805988647b2566bdf14eaaafced5611
ABAYOMI OLUJOBI <abayomi@ABAYOMIs-iMac.local>
2021-04-18 21:44:45 -0400

    Add Total Votes to PyPoll Scripts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,3 +12,49 @@ This script will analyze the votes and calculate the following:
 
 Author: Abayomi Olujobi
 """
+import os
+import csv
+
+path = "/Users/abayomi/GitHubs/Python-Challenge/PyPoll/"
+election_data = os.path.join(path,"Resources", "election_data.csv")
+
+def formatted(number):
+    #Formats large numbers for more readability
+    format_number = "{:,}".format(number)
+    return format_number
+
+def candidates_list(list1):
+    #Returns a list of the unique candidates
+    each_candidate = []
+    for i in list1:
+        if i not in each_candidate:
+            each_candidate.append(i)
+    return each_candidate
+
+with open(election_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    next(csvreader)
+
+    vote_count = 0
+    candidates = []
+    dates = []
+    for row in csvreader:
+        vote_count += 1
+        
+        #Converting Candidate column into a list
+        candidates.append(row[2])
+
+
+    print('\nElection Results')
+    print(f'-------------------------')
+    print(f'Total Votes: {formatted(vote_count)}')
+    print(f'-------------------------')
+
+    #Getting the names of candidates
+    unique_candidates = candidates_list(candidates)
+    print(unique_candidates)
+    
+    
+        
+
+
